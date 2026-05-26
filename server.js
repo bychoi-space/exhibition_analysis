a41256fabe3e203846830fa24a55df54792c9a60
choiby01 <choiby01@gmail.com>
2026-05-26 18:26:51 +0900

    fix: Add explicit style.css GET route to force Vercel to bundle the CSS stylesheet

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static(__dirname));
 
+app.get('/style.css', (req, res) => {
+  res.setHeader('Content-Type', 'text/css');
+  res.sendFile(path.join(__dirname, 'style.css'));
+});
+
 // --- Simulated In-Memory Database & Seed Engine ---
 let eventsDatabase = [];
 
